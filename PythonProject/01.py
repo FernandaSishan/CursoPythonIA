f6e7f2ae2de068c7b043de8f82434558bb88b7da
unknown <carolinef409@gmail.com>
2026-05-12 19:26:54 -0300

    first commit

---
+++ PythonProject/01.py
@@ -0,0 +1,7 @@
+ingredientes = ["farinha", "açúcar", "ovos", "leite"]
+
+for ingrediente in ingredientes:
+    print(f'Incluindo: {ingrediente}')
+
+for passo in range(1,6):
+    print(f'Passo: {passo}: Complete a tarefa.')
---
+++ PythonProject/02.py
@@ -0,0 +1,4 @@
+desafios = ["Fundamentos de programação", "Desenvolvendo API´s", "Teste automatizados"]
+
+for i, desafio in enumerate(desafios, start=1):
+    print(f"{i}: {desafio}")
